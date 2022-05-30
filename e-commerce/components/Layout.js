c05e5940b35fb11d7a0ba671d08d8f1339e10bbb
nunexxito <snunez.music@gmail.com>
2022-05-30 17:38:28 -0500

    ajustando archivos de diseño

--- e-commerce/components/Layout.js
+++ e-commerce/components/Layout.js
@@ -1,5 +1,5 @@
-import Footer from "./Footer";
-import Navbar from "./Navbar";
+import Footer from "./Footer/Index";
+import Navbar from "./Navbar/Index";
 
 export default function Layout({children}) {
     return(
